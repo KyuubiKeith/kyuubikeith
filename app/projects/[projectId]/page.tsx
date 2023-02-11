efe1971b2a4ef6a39664774aa8e04c178bc94b77
KyuubiKeith <havoxworx@gmail.com>
2023-02-11 10:29:52 +0300

    Fixed deployment errors
    checks still failing, so I removed the checkly integration from the project to look at later

---
+++ app/projects/[projectId]/page.tsx
@@ -0,0 +1,76 @@
+// ==================== Imports =====================//
+
+// NextJS
+import Image from 'next/image'
+import Link from 'next/link'
+import Project_ from './project'
+
+// Contentful Client
+import { Client } from '../../../source/organisms/content/contentful'
+import { TypeProjectsFields } from "@/source/organisms/content/contentful";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, Document } from '@contentful/rich-text-types'
+import { Fragment } from 'react';
+
+// ==================== Imports =====================//
+
+//
+
+// ==================== Query =====================//
+// ==================== Query =====================//
+
+//
+
+// ==================== Render =====================//
+
+// export async function generateStaticParams({params}) {
+
+
+//   const { items } = await Client.getEntries({
+//     content_type: 'projects'
+//   })
+//   console.log(projectId)
+
+// }
+
+export default async function Project() {
+    
+  const { items } = await Client.getEntries({
+    content_type: 'projects'
+  })
+  // console.log(items)
+  // console.log(props)
+  // console.log(params['projectId'])
+
+  const {
+    projectId,
+    name,
+    slug,
+    logo,
+    client,
+    work,
+    featured,
+    featuredImageLink,
+    caseStudy
+  } = items[0].fields as TypeProjectsFields
+
+  // console.log(projectId)
+
+  return (
+    <Project_
+      key={projectId}
+      projectId={projectId}
+      client={client}
+      slug={slug}
+      name={name}
+      work={work}
+      logo={logo}
+      featured={featured}
+      caseStudy={caseStudy}
+      featuredImageLink={featuredImageLink}
+    />
+  )
+}
+
+// ==================== Render =====================//
+
---
+++ app/projects/page.tsx
@@ -0,0 +1,54 @@
+// ==================== Imports =====================//
+
+// NextJS
+import Image from 'next/image'
+import Link from 'next/link'
+
+// Contentful Client
+import { Client } from '@/source/organisms/content/contentful'
+import { TypeProjectsFields } from '@/source/organisms/content/contentful'
+
+import Project from './project'
+
+// ==================== Imports =====================//
+
+//
+
+// ==================== Render =====================//
+
+export default async function Projects() {
+
+  const data = await Client.getEntries({
+    content_type: 'projects'
+  })
+
+  const projectData: TypeProjectsFields = data.items.map(
+    (project) => project.fields
+  )
+
+  // console.log(projectData)
+
+  return (
+
+    <>
+      {projectData
+        .sort((a: { projectId: number }, b: { projectId: number }) => {
+          return a.projectId - b.projectId
+        })
+        .map((project: TypeProjectsFields) => (
+          <Project
+            key={project.projectId}
+            projectId={project.projectId}
+            client={project.client}
+            slug={project.slug}
+            name={project.name}
+            work={project.work}
+          />
+        ))}
+    </>
+
+  )
+}
+
+// ==================== Render =====================//
+
